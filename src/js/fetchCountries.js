1ab49b7bc701f8cf96a7a3dc3e92d9da405c57ea
Fosa1990 <fosa1990@gmail.com>
2021-10-16 16:28:30 +0300

    add cath() to fetch()

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v2/name/';
 
 export default function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}${searchQuery}`).then(response => response.json());
+  return fetch(`${BASE_URL}${searchQuery}`)
+    .then(response => response.json())
+    .catch(error => alert(error));
 }
