29b577314a721541d0b167c5aadba1184106561d
Fosa1990 <fosa1990@gmail.com>
2021-10-27 22:22:51 +0300

    deleted json() from fetch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,5 @@
 const BASE_URL = 'https://restcountries.com/v2/name/';
 
-export default function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}${searchQuery}`)
-    .then(response => response.json())
-    .catch(error => alert(error));
+export default async function fetchCountries(searchQuery) {
+  return await fetch(`${BASE_URL}${searchQuery}`);
 }
